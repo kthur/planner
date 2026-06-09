687a39ce8519277e50a68dbfa46f2a14f92af42a
KYUNGTAK HUR <kyungtak.hur@gmail.com>
2026-06-10 01:36:37 +0900

    Fix missing mutableStateOf import in MainActivity

--- app/src/main/java/com/planner/tracker/MainActivity.kt
+++ app/src/main/java/com/planner/tracker/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
