507348f7cfac63fd261542314d4b291205375254
KYUNGTAK HUR <kyungtak.hur@gmail.com>
2026-06-11 02:31:27 +0900

    chore: add firebase configuration and google-services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+	id("com.google.gms.google-services")
 }
 
 android {
@@ -44,6 +45,9 @@ android {
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.01.00")
     implementation(composeBom)
+	
+	implementation(platform("com.google.firebase:firebase-bom:34.14.0"))
+	implementation("com.google.firebase:firebase-analytics")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
